6e62c1e6562fdf0588fbf319e4d643f8e4e2cba7
badStephane <badstephane14@gmail.com>
2025-03-10 14:45:18 +0000

    add StringLength exercise solution

---
+++ StringLength/StringLength.java
@@ -0,0 +1,10 @@
+public class StringLength {
+    public static int getStringLength(String s) {
+        if (s == null) {
+            return 0;
+        }  else {
+            return s.length();
+        }
+    }
+}
+
