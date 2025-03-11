9a5f75f8afef1443f5d5245300727bc02d26a95e
badStephane <badstephane14@gmail.com>
2025-03-11 17:50:53 +0000

    add StringReplace exercise solution

--- StringReplace/StringReplace.java
+++ StringReplace/StringReplace.java
@@ -1,5 +1,8 @@
 public class StringReplace {
-    public static String replace(String s, String target, String replacement) {
+    public static String replace(String s, char target, char replacement) {
+        return s.replace(target,replacement);
+    }
+    public static String replace(String s, CharSequence target, CharSequence replacement) {
         return s.replace(target,replacement);
     }
 }
