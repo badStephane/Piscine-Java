9b63aa0568bd934270ad050b24cd50ce367135a0
badStephane <badstephane14@gmail.com>
2025-03-13 02:30:32 +0000

    add Palindrome exercise solution

---
+++ Palindrome/Palindrome.java
@@ -0,0 +1,33 @@
+public class Palindrome {
+    public static boolean isPalindrome(String s) {
+        if (s == null) {
+            return false;
+        }
+
+        if (s.isEmpty()){
+            return true;
+        }
+
+        String cleanS = s.toLowerCase(); 
+        int left = 0;
+        int right = cleanS.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(cleanS.charAt(left)) && !Character.isWhitespace(cleanS.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(cleanS.charAt(right)) && !Character.isWhitespace(cleanS.charAt(right))) {
+                right--;
+            }
+
+            if (cleanS.charAt(left) != cleanS.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+   
+}
+   
