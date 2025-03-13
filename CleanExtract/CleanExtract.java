66142097d409cdc683a1b0660bb0c40fedcdcc46
badStephane <badstephane14@gmail.com>
2025-03-13 02:28:36 +0000

    add CleanExtract exercise solution

---
+++ CleanExtract/CleanExtract.java
@@ -0,0 +1,43 @@
+public class CleanExtract {
+    public static String extract(String s) {
+
+        String[] substrings = s.split("\\|");
+        StringBuilder result = new StringBuilder(); 
+
+        for (String sub : substrings) {
+
+            if (sub.startsWith(".") && sub.endsWith(".")) {
+                int firstDot = sub.indexOf(".");
+                int lastDot = sub.lastIndexOf(".");
+                String str = sub.substring(firstDot , lastDot).trim();
+                if (!str.isEmpty()) { 
+                    result.append(" ").append(str);
+                }
+            } else if (sub.contains(".")) {
+                int index = sub.indexOf(".");
+                String str = sub.substring(index + 1).trim();
+                if (!str.isEmpty()) { 
+                    result.append(" ").append(str);
+                }
+            }
+        }
+
+        for (int i = 0; i < result.length()-1; i++) {
+            if (result.charAt(i) == '.') {
+                result.deleteCharAt(i);
+                i--; 
+            }
+        }
+
+        String output = result.toString().replaceAll("\\s+", " ").trim();
+
+        if (output.equals(".")) {
+            return "";
+        }
+        return output;
+            
+            
+       
+
+    }
+}
