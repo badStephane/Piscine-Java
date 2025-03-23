6669a6f684c159be972282668891f06eed797ea4
badStephane <badstephane14@gmail.com>
2025-03-23 02:12:21 +0000

    add SetEquals

---
+++ SetEquals/SetEquals.java
@@ -0,0 +1,16 @@
+import java.util.Set;
+
+public class SetEquals {
+    public static boolean areSetsEqual(Set<String> set1, Set<String> set2) {
+        if (set1 == null && set2 == null) {
+            return true;
+        }
+        if (set1.isEmpty() && set2.isEmpty()) {
+            return true;
+        }
+        if (set1 == null || set2 == null) {
+            return false;
+        }
+        return set1.equals(set2);
+    }
+}
