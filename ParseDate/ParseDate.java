ac36647a8bf70b6ea0e0eea3de70c7dd9da89835
badStephane <badstephane14@gmail.com>
2025-03-24 12:12:18 +0000

    add ParseDate exercise solution

--- ParseDate/ParseDate.java
+++ ParseDate/ParseDate.java
@@ -1,22 +1,44 @@
+// package ParseDate;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class ParseDate {
 
     public static LocalDateTime parseIsoFormat(String stringDate) {
-        // your code here
-        return LocalDateTime.parse(stringDate);
+        if (stringDate == null) {
+            return null;
+        }
+       return LocalDateTime.parse(stringDate);
     }
 
     public static LocalDate parseFullTextFormat(String stringDate) {
-        // your code here
-        return LocalDate.parse(stringDate);
+        if (stringDate == null) {
+            return null;
+        }
+        return LocalDate.parse(stringDate, DateTimeFormatter.ofPattern("EEEE d MMMM yyyy", Locale.FRENCH));
     }
 
     public static LocalTime parseTimeFormat(String stringDate) {
-        // your code here
-        return LocalTime.parse(stringDate);
+        if(stringDate == null) {
+            return null;
+        }
+        String[] parts = stringDate.split(", ");
+        String[] hourPart = parts[0].split(" ");
+        String[] minutePart = parts[1].split(" et ");
+        
+        int hour = Integer.parseInt(hourPart[0]);
+        int minute = Integer.parseInt(minutePart[0].split(" ")[0]);
+        int second = Integer.parseInt(minutePart[1].split(" ")[0]);
+
+        if (stringDate.contains("du soir") && hour < 12) {
+            hour += 12;
+        }
+
+        return LocalTime.of(hour, minute, second);
     }
 
 }
