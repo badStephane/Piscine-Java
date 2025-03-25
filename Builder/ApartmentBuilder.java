a44929c907de48655d09ad094232b924b902e4b6
badStephane <badstephane14@gmail.com>
2025-03-25 15:17:40 +0000

    Add Builder exercise solution

---
+++ Builder/ApartmentBuilder.java
@@ -0,0 +1,31 @@
+public class ApartmentBuilder implements HousingBuilder {
+    private Apartment apartment;
+
+    public ApartmentBuilder() {
+        this.apartment = new Apartment();
+    }
+
+    public HousingBuilder setSize(int size) {
+        this.apartment.setSize(size);
+        return this;
+    }
+
+    public HousingBuilder setPrice(int price) {
+        this.apartment.setPrice(price);
+        return this;
+    }
+
+    public HousingBuilder setRooms(int rooms) {
+        this.apartment.setRooms(rooms);
+        return this;
+    }
+
+    public HousingBuilder setName(String name) {
+        this.apartment.setName(name);
+        return this;
+    }
+
+    public Housing build() {
+        return this.apartment;
+    }
+}
---
+++ Builder/HouseBuilder.java
@@ -0,0 +1,31 @@
+public class HouseBuilder implements HousingBuilder {
+    private Housing house;
+
+    public HouseBuilder() {
+        this.house = new House();
+    }
+
+    public HousingBuilder setSize(int size) {
+        this.house.setSize(size);
+        return this;
+    }
+
+    public HousingBuilder setPrice(int price) {
+        this.house.setPrice(price);
+        return this;
+    }
+
+    public HousingBuilder setRooms(int rooms) {
+        this.house.setRooms(rooms);
+        return this;
+    }
+
+    public HousingBuilder setName(String name) {
+        this.house.setName(name);
+        return this;
+    }
+
+    public Housing build() {
+        return this.house;
+    }
+}
