f69a3df472c7c1eb6243d9199383848958f2b763
badStephane <badstephane14@gmail.com>
2025-03-25 13:21:37 +0000

    add DifferenceBetweenDate exercise solution

---
+++ DifferenceBetweenDate/DifferenceBetweenDate.java
@@ -0,0 +1,40 @@
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+
+public class DifferenceBetweenDate {
+
+    public static Duration durationBetweenTime(LocalTime localTime1, LocalTime localTime2) {
+        if(localTime1 == null || localTime2 == null) {
+            return null;
+        }
+        Duration duration = Duration.between(localTime1, localTime2);
+        if(duration.isNegative()){
+            return duration.abs();
+        }
+        return duration;
+    }
+
+    public static Period periodBetweenDate(LocalDate date1, LocalDate date2) {
+        if(date1 == null || date2 == null) {
+            return null;
+        }
+        Period period = Period.between(date1, date2);
+        if(period.isNegative()){
+            return period.negated();
+        }
+        return period;
+    }
+
+    public static Long numberOfHoursBetweenDateTime(LocalDateTime dateTime1, LocalDateTime dateTime2) {
+        // your code here
+        if(dateTime1 == null || dateTime2 == null) {
+            return null;
+        }
+        Duration duration = Duration.between(dateTime1, dateTime2);
+        return Math.abs(duration.toHours());
+    }
+
+}
